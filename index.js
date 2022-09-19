38831f69a3c28509386c1f7b91b44e18e8b47db2
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-09-18 22:31:23 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var num1 = 6    //number data type
+var num2 = 2    //number data type
+console.log(num1 - num2); //number data type
+
+const x = 'seth';
+const y = 'charles';
+
+console.log(`${y} is ${y.length - x.length} characters longer than ${x}`);
+
+
+
+
+
+
+var num1 = 'HI'
+var num2 = 'Hi'
+var num3 = 'hi'
+
+
+alert ('welcome!')
+let num = prompt("respond with (hi or HI or Hi)");
+switch  (num) {
+    case 'HI':
+alert('dont yell at me!!');
+    break;
+    case 'Hi':
+alert('Hello!');
+    break;
+    case 'hi':
+alert('speak up!!!!!');
+    break;
+        default:
+alert('How did you manage to not follow directions. you really suck at following directions. SAD');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
