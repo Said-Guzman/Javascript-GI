91591c40563946c0ca5f4d69a1a80e695b47ddbb
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-09-19 03:12:56 -0400

    calculator

--- index.js
+++ index.js
@@ -21,19 +21,40 @@ alert ('welcome!')
 let num = prompt("respond with (hi or HI or Hi)");
 switch  (num) {
     case 'HI':
-alert('dont yell at me!!');
+alert('Dont yell at me!!');
     break;
     case 'Hi':
 alert('Hello!');
     break;
     case 'hi':
-alert('speak up!!!!!');
+alert('Speak up!!!!!');
     break;
         default:
 alert('How did you manage to not follow directions. you really suck at following directions. SAD');
 }
 
+const operator = prompt('Enter symbol(+ , - , * , /):');
+// parsefloat makes it into a string if needed
+const num4 = parseFloat(prompt('First Number:'));
+const num5 = parseFloat(prompt('Sencond Number:'));
 
+let result;
+if(operator == '+'){
+    result = num4 + num5;}
+
+else if (operator == '-'){
+    result = num4 - num5;
+    }
+
+else if (operator == '*'){
+    result = num4 * num5;
+}
+else if(operator == '/')
+{
+result = num4 / num5;
+}
+
+console.log(`${num4} ${operator} ${num5} = ${result}`);
 
 
 
